c7a6aba900294fb5cc9446e55c13b4fda54bfa60
Aaron <aarontburnham@hotmail.com>
2024-04-30 20:48:23 -0700

    Modifed home to match live

--- src/built_ins/home_module/HomeRenderer.ts
+++ src/built_ins/home_module/HomeRenderer.ts
@@ -3,6 +3,11 @@
     const MODULE_PROCESS_NAME = MODULE_NAME.toLowerCase() + "-process";
     const MODULE_RENDERER_NAME = MODULE_NAME.toLowerCase() + "-renderer"
 
+    const sendToProcess = (eventType: string, ...data: any): void => {
+        window.parent.ipc.send(MODULE_PROCESS_NAME.toLowerCase(), eventType, ...data);
+    }
+
+    sendToProcess("init");
 
     const fullDate: HTMLElement = document.getElementById("full-date");
     const abbreviatedDate: HTMLElement = document.getElementById("abbreviated-date");
@@ -10,8 +15,6 @@
     const standardTime: HTMLElement = document.getElementById("standard-time");
     const militaryTime: HTMLElement = document.getElementById("military-time");
     
-    
-    window.parent.ipc.send(MODULE_PROCESS_NAME, "init");
     window.parent.ipc.on(MODULE_RENDERER_NAME, (_, eventType: string, data: any[]) => {
         data = data[0]; // Data is wrapped in an extra array.
         switch(eventType) {
